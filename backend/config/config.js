5435a102eb2c654dae40c13db72cba519536d84b
Rupert <dsouza11anish@gmail.com>
2024-08-09 12:35:37 +0530

    Implimented Serverless Postgre SQL

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,18 +1,17 @@
 // config/config.js
 module.exports = {
-    development: {
-      username: 'root',
-      password: null,
-      database: 'institute',
-      host: '127.0.0.1',
-      dialect: 'mysql'
-    },
-    production: {
-      username: 'root',
-      password: null,
-      database: 'database_production',
-      host: '127.0.0.1',
-      dialect: 'mysql'
-    }
-  };
-  
+  development: {
+    username: "anish",
+    password: "Hq4XMsumntcuiRTHbrZCtA",
+    database: "institute",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,11 +1,31 @@
 // config/sequelize.js
-const { Sequelize } = require('sequelize');
-const config = require('./config')[process.env.NODE_ENV || 'development'];
+// config/sequelize.js
+const { Sequelize } = require("sequelize");
+const pg = require("pg"); // Add this line to import pg
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect
+const connectionString = process.env.POSTGRESQL_URL;
+
+const sequelize = new Sequelize(connectionString, {
+  dialect: "postgres",
+  dialectModule: pg, // Add this line to explicitly use pg
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
+// const config = require("./config")[process.env.NODE_ENV || "development"];
 
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   {
+//     host: config.host,
+//     dialect: config.dialect,
+//   }
+// );
+
+// module.exports = sequelize;
