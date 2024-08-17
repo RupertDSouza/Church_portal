e3d244e0b2f321ab6bed135fd2297e0286105de9
Rupert <dsouza11anish@gmail.com>
2024-08-17 17:54:57 +0530

    Created page for parish council

---
+++ public/js/parishCouncil.js
@@ -0,0 +1,50 @@
+async function loadAssociationInfo(containerId) {
+  try {
+    const response = await fetch("/app/parishcouncil/read", {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+
+    const data = await response.json();
+
+    const container = document.getElementById(containerId);
+    container.innerHTML = ""; // Clear existing content
+
+    if (data.length === 0) {
+      return;
+    }
+
+    for (let index = 0; index <= data.length - 1; index++) {
+      const item = data[index];
+
+      const imageUrl = item.image.replace(/^.*\/public\//, "../");
+      const itemHtml = `
+                      <div class="col-md-3">
+                          <div class="popular_item">
+                            <div class="imageAndInfo">
+                                <div class="text-center parish-council_item">
+                                    <div class="parish-council_item_image">
+                                        <img src="${imageUrl}" alt="${item.name}" />
+                                    </div>
+                                    <div class="parish-council_item_title">${item.name}</div>
+                                    <div class="parish-council_item_text">
+                                        <h6>${item.designation}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                          </div>
+                      </div>
+            `;
+      container.insertAdjacentHTML("beforeend", itemHtml);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadAssociationInfo("parish-council-container");
+});
